1d8ae2752ca60c5015126b5740a4d24f7a375012
Matthew Radcliffe <mradcliffe@softpixel.com>
2018-03-24 08:56:45 -0400

    Fine. I'll remove everything because zip -x isn't working

--- scripts/get_packages.sh
+++ scripts/get_packages.sh
@@ -7,21 +7,21 @@ GIT_WIN="https://github.com/git-for-windows/git/releases/download/v2.16.2.window
 SEVENZIP_WIN="https://www.7-zip.org/a/7z1801-x64.exe"
 
 # Create directories.
-mkdir -vp ${RELEASE_DIR}/Windows
-mkdir -vp ${RELEASE_DIR}/MacOS
-mkdir -vp ${RELEASE_DIR}/Linux
+mkdir -vp ${DL_DIR}/Windows
+mkdir -vp ${DL_DIR}/MacOS
+mkdir -vp ${DL_DIR}/Linux
 
 # Download Docker for MacOS.
-curl -#SL -o ${RELEASE_DIR}/MacOS/Docker.dmg ${DOCKER_MACOS} || exit 1
+curl -#SL -o ${DL_DIR}/MacOS/Docker.dmg ${DOCKER_MACOS} || exit 1
 
 # Download Docker for Windows and Docker ToolBox and other dependencies.
-curl -#SL -o ${RELEASE_DIR}/Windows/Docker\ for\ Windows\ Installer.exe ${DOCKER_WIN10} || exit 1
-curl -#SL -o ${RELEASE_DIR}/Windows/DockerToolbox.exe ${DOCKER_TOOLBOX} || exit 1
-curl -#SL -o ${RELEASE_DIR}/Windows/7za.exe ${SEVENZIP_WIN} || exit 1
-curl -#SL -o ${RELEASE_DIR}/Windows/PortableGit-2.16.2-64-bit.7z.exe ${GIT_WIN} || exit 1
+curl -#SL -o ${DL_DIR}/Windows/Docker\ for\ Windows\ Installer.exe ${DOCKER_WIN10} || exit 1
+curl -#SL -o ${DL_DIR}/Windows/DockerToolbox.exe ${DOCKER_TOOLBOX} || exit 1
+curl -#SL -o ${DL_DIR}/Windows/7za.exe ${SEVENZIP_WIN} || exit 1
+curl -#SL -o ${DL_DIR}/Windows/PortableGit-2.16.2-64-bit.7z.exe ${GIT_WIN} || exit 1
 
 # Write out some instructions for Linux users.
-cat <<EOF > "${RELEASE_DIR}/Linux/README"
+cat <<EOF > "${DL_DIR}/Linux/README"
 You should follow the instructions to download Docker for your distribution at
 
    https://www.docker.com/community-edition
